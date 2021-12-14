75d69c215c8ee48f0ac06d00d31991e0855264f2
aakzsh <aakashferrari@gmail.com>
2021-12-14 10:55:36 +0530

    add calc file

---
+++ calculate.py
@@ -0,0 +1,55 @@
+
+print("enter area of land in metres")
+area = int(input())
+
+emissions = 0
+
+print("How many dairy animals do you own?")
+animals = int(input())
+
+emissions+= animals*773.391273
+
+print("how many tractors do you have?")
+tractors = int(input())
+
+print("1. petrol\n2. diesel\n3. none?")
+type = int(input())
+
+if type==1:
+    emissions+=tractors* (20000/6) * 9 * 3.78 * 0.001
+elif type==2:
+    emissions+=tractors* (20000/6) * 11 * 3.78 * 0.001
+else:
+    pass
+
+emissions2 = 0
+
+print("do you use fertilizers? y/n")
+
+s = input()
+if s=='y':
+    print("what kinda? \n1: organic\n2: inorganic")
+    type_fert = int(input())
+    if type_fert!=1:
+        emissions2+=area*(0.28/25)*2
+    
+print("whats the age of your soil? in yrs")
+
+years = int(input())
+emissions2+=years*0.5*area
+
+print("what kinda crop rotation do you follow?\n1: extensive\n2: medium\n3: rare\n4: none")
+rotation = int(input())
+
+if rotation==1:
+    emissions+=0.65*emissions2
+elif rotation==2:
+    emissions+=0.75*emissions2
+elif rotation==3:
+    emissions+=0.90*emissions2
+    
+print(f'Your carbon footprint is around {round(emissions,2)} tonnes per year')
+    
+    
+    
+    
